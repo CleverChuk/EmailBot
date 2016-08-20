437b34eb8314bfab2098a4638567e2c0e09f2bc8
Clever <Chubiyke@gmail.com>
2016-08-20 15:58:09 -0400

    Bat file to start EmailBot after configuration

---
+++ emailBot/WindowsBat/userBot.bat
@@ -0,0 +1,16 @@
+rem Author: CleverChuk
+rem for Windows OS
+rem User script for running EmailBot
+@echo off
+rem Batch program for running EmailBot driver.py
+@echo on
+@echo "Starting MailBot"
+@echo off
+rem This command will only work if python is in your PATH
+Python C:\Users\_username_\Desktop\emailBot\driver.py
+
+rem if python is not in your PATH add it or do
+rem C:\python3 C:\Users\_username_\Desktop\emailBot\driver.py
+@echo on
+@echo "Done"
+
